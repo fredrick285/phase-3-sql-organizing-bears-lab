0297aa0b9653ed5089b5cb06378cecbc605dc07b
Fredrick <fredrick.lokidor@student.moringaschool.com>
2022-08-28 22:20:34 +0300

    COMPLETE

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+  id INT PRIMARY KEY IDENTITY,
+  name TEXT,
+  age INTEGER,
+  sex TEXT,
+  temperament TEXT,
+  alive BOOLEAN
+
+);
---
+++ lib/insert.sql
@@ -0,0 +1,25 @@
+INSERT INTO bears(name,sex)VALUES
+(
+    "Mr.Chocolate",M
+),
+(
+    "Rowdy",M
+),
+(
+    "Tabitha" ,F
+),
+(
+    "Sergeant Brown",F
+),
+(
+    "Melissa",F
+),
+(
+    "Grinch", M
+)
+(
+    "Wendy", F
+),
+(
+    Null ,F
+)
